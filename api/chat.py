ead143b4063bc7e0a1e6e71daf4428e6279abc2b
Rimander <ferminjimenez93@gmail.com>
2021-05-22 21:23:42 +0200

    command chat

--- api/chat.py
+++ api/chat.py
@@ -4,6 +4,85 @@ import time
 import websocket
 import logging
 
+from api.config import oauth, nick, channel
+
+'''
+# color: red, blue, grey....
+# direction: up, down, left, right
+def on_move_event(color, direction):
+    pass
+
+
+async def run_app():
+    CommandChat(debug=False, on_move_event=on_move_event)
+
+
+asyncio.get_event_loop().run_until_complete(run_app())
+'''
+
+
+class CommandChat:
+
+    def __init__(self, on_move_event=None, debug=False):
+        self.commands = {}
+        self.lock = threading.Lock()
+        self.on_move_event = on_move_event
+        threading.Thread(target=self.timming_group).start()
+        self.chat = ChatWrapper(oauth, nick, channel, self.on_command, self.on_error, debug)
+        self.chat.start()
+
+    def on_command(self, nick, command):
+        # Standard replaces
+        while "  " in command:
+            command = command.replace("  ", " ")
+
+        # Start with !move
+        if not command.startswith("!move"):
+            return
+
+        color = command.split(" ")[1].strip()
+        direction = command.split(" ")[2].strip()
+
+        # Valid color
+        if color not in ['red', 'blue']:
+            return
+
+        # Valir direction
+        if direction not in ['up', 'down', 'left', 'right']:
+            return
+
+        key = '{} {}'.format(color, direction)
+
+        self.lock.acquire()
+        try:
+            if key not in self.commands.keys():
+                self.commands[key] = 0
+
+            self.commands[key] = self.commands[key] + 1
+
+        except:
+            logging.error("Count commands")
+        self.lock.release()
+
+    def on_error(self, content):
+        print("Error: {}".format(content))
+
+    # Calculate next move and emit event
+    def _next_move(self):
+        # Has commands
+        if len(self.commands.values()) > 0:
+            next_move = max(self.commands, key=self.commands.get)
+            color = next_move.split(" ")[0]
+            direction = next_move.split(" ")[1]
+            self.on_move_event(color, direction)
+            self.commands = {}
+
+    # Each 10 seconds, next move
+    def timming_group(self):
+        while True:
+            time.sleep(10)
+            self._next_move()
+
 
 class ChatWrapper:
 
